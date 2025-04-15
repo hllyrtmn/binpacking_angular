8889c091cb2d3c0fec215c7efe5c116349be7b57
hllyrtmn <halilyurtman@gmail.com>
2025-04-15 19:42:06 +0300

    updated

--- src/app/admin/components/dashboard/stepper/components/invoice-upload/invoice-upload.component.ts
+++ src/app/admin/components/dashboard/stepper/components/invoice-upload/invoice-upload.component.ts
@@ -12,13 +12,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RepositoryService } from '../../services/repository.service';
-import { Observable, switchMap, tap,catchError,finalize } from 'rxjs';
+import { Observable, switchMap, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { mapToOrderDetailDtoList } from '../../../../../../models/mappers/order-detail.mapper';
 import { OrderDetailDto } from '../../../../../../models/dtos/order-detail-dto.interface';
-import { ModelTableComponent } from "../../../../../../components/model-table/model-table.component";
+import { ModelTableComponent } from '../../../../../../components/model-table/model-table.component';
 import { ToastService } from '../../../../../../services/toast.service';
-
 
 @Component({
   selector: 'app-invoice-upload',
@@ -31,8 +30,8 @@ import { ToastService } from '../../../../../../services/toast.service';
     MatTableModule,
     MatInputModule,
     CommonModule,
-    ModelTableComponent
-],
+    ModelTableComponent,
+  ],
   templateUrl: './invoice-upload.component.html',
   styleUrl: './invoice-upload.component.scss',
 })
@@ -45,7 +44,6 @@ export class InvoiceUploadComponent implements OnInit {
   uploadForm: FormGroup;
   file: File | null = null;
   isLoading = false;
-
 
   orderDetailDtoList: OrderDetailDto[] = [];
   dataSource = new MatTableDataSource<OrderDetailDto>(this.orderDetailDtoList);
@@ -61,13 +59,11 @@ export class InvoiceUploadComponent implements OnInit {
 
   constructor() {
     this.uploadForm = this._formBuilder.group({
-      fileInput: ['', Validators.required]
+      fileInput: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
@@ -78,11 +74,17 @@ export class InvoiceUploadComponent implements OnInit {
   }
 
   validateFile(file: File): void {
-    const validTypes = ['application/pdf', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+    const validTypes = [
+      'application/pdf',
+      'application/vnd.ms-excel',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    ];
     const maxSize = 10 * 1024 * 1024;
 
     if (!validTypes.includes(file.type)) {
-      this.toastService.error('Geçersiz dosya türü. Lütfen bir PDF veya Excel dosyası yükleyin.');
+      this.toastService.error(
+        'Geçersiz dosya türü. Lütfen bir PDF veya Excel dosyası yükleyin.'
+      );
       this.file = null;
       this.uploadForm.get('fileInput')?.reset();
       return;
@@ -107,31 +109,25 @@ export class InvoiceUploadComponent implements OnInit {
     this.toastService.info('Dosya yükleniyor...');
     this.repositoryService
       .uploadFile(this.file)
+      .pipe(
+        tap((response) => {
+          this.repositoryService.setOrderId(response.order || '');
+          this.toastService.success('Dosya başarıyla yüklendi.');
+          this.toastService.info('Dosya işleniyor...');
+        }),
+        switchMap((response) => this.processFile(response.id))
+      )
       .subscribe({
-        next: (response) => {
-          if (response.order) {
-            this.repositoryService.setOrderId(response.order);
-            this.toastService.success('Dosya başarıyla yüklendi.');
-            this.toastService.info('Dosya işleniyor...');
-
-            this.processFile(response.id).subscribe({
-              next: () => {
-                this.updateDataSource();
-                this.toastService.success('Dosya başarıyla işlendi.');
-                this.resetForm();
-                this.isLoading = false;
-              },
-              error: (processError) => {
-                this.handleError(processError);
-                this.isLoading = false;
-              }
-            });
-          }
-        },
-        error: (uploadError) => {
-          this.handleError(uploadError);
+        next: () => {
+          this.updateDataSource();
+          this.toastService.success('Dosya başarıyla işlendi.');
+          this.resetForm();
           this.isLoading = false;
-        }
+        },
+        error: (err) => {
+          this.handleError(err);
+          this.isLoading = false;
+        },
       });
   }
 
@@ -141,7 +137,8 @@ export class InvoiceUploadComponent implements OnInit {
   }
 
   private updateDataSource(): void {
-    const orderDetails = this.repositoryService.orderDetailResource.value().results;
+    const orderDetails =
+      this.repositoryService.orderDetailResource.value().results;
     if (orderDetails) {
       this.orderDetailDtoList = mapToOrderDetailDtoList(orderDetails);
       this.dataSource.data = this.orderDetailDtoList;
@@ -152,7 +149,9 @@ export class InvoiceUploadComponent implements OnInit {
   private handleError(error: any): void {
     console.error('Error:', error);
     this.repositoryService.addErrors(error);
-    this.toastService.error('Dosya yükleme veya işleme sırasında bir hata oluştu.');
+    this.toastService.error(
+      'Dosya yükleme veya işleme sırasında bir hata oluştu.'
+    );
   }
 
   private resetForm(): void {
@@ -166,5 +165,4 @@ export class InvoiceUploadComponent implements OnInit {
   onDelete(id: string) {
     console.log('Delete model with ID:', id);
   }
-
 }
