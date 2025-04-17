1080a75925dd416bcdb5f2d24d33c07d49bd25c7
hllyrtmn <halilyurtman@gmail.com>
2025-04-17 10:59:10 +0300

    break point

--- src/app/admin/components/dashboard/stepper/components/pallet-control/pallet-control.component.ts
+++ src/app/admin/components/dashboard/stepper/components/pallet-control/pallet-control.component.ts
@@ -19,22 +19,22 @@ import {
   CdkDropList,
 } from '@angular/cdk/drag-drop';
 
-export interface PeriodicElement {
-  id: number;
+export interface Product {
+  name: string;
   position: number;
-  count: number;
   weight: number;
-  width: number;
-  depth: number;
+  price: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { id:1 ,position: 1, count: 120, weight: 1.0079, width: 9.0122, depth: 1.0079 },
-  { id:2 ,position: 2, count: 145, weight: 4.0026, width: 9.0122, depth: 1.0079 },
-  { id:3 , position: 3, count: 55, weight: 6.941, width: 9.0122, depth: 1.0079 },
-  { id:4 ,position: 4, count: 45, weight: 9.0122, width: 9.0122, depth: 1.0079 },
-  { id:5 ,position: 5, count: 22, weight: 10.811, width: 9.0122, depth: 1.0079 },
-];
+interface Pallet {
+  id: string;
+  products: Product[];
+}
+
+interface Package {
+  id: string;
+  pallets: Pallet[];
+}
 
 @Component({
   selector: 'app-pallet-control',
@@ -53,18 +53,33 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrl: './pallet-control.component.scss',
 })
 export class PalletControlComponent implements OnInit {
-
   @Input() order_id!: string;
-  items = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  products: Product[] = [
+    {position: 1, name: 'Radiator 1', weight: 1.0079, price: '120H'},
+    {position: 2, name: 'Radiator 2', weight: 4.0026, price: '111He'},
+    {position: 3, name: 'Radiator 3', weight: 6.941, price: '222Li'},
+    {position: 4, name: 'Radiator 4', weight: 9.0122, price: '33Be'},
+    {position: 5, name: 'Radiator 5', weight: 10.811, price: '45B'},
+    {position: 6, name: 'Radiator 6', weight: 12.0107, price: '67C'},
+    {position: 7, name: 'Radiator 7', weight: 14.0067, price: '87N'},
+    {position: 8, name: 'Radiator 8', weight: 15.9994, price: '65O'},
+    {position: 9, name: 'Radiator 9', weight: 18.9984, price: '43F'},
+    {position: 10, name: 'Radiator 10', weight: 20.1797, price: '34Ne'},
+  ];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  packages = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+
+  pallets = ['Pallet 1', 'Pallet 2', 'Pallet 3', 'Pallet 4', 'Pallet 5', 'Pallet 6', 'Pallet 7', 'Pallet 8', 'Pallet 9', 'Pallet 10'];
+
+
   drop1(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.packages, event.previousIndex, event.currentIndex);
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Product[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -76,17 +91,6 @@ export class PalletControlComponent implements OnInit {
       );
     }
   }
-
-  displayedColumns: string[] = [
-    'position',
-    'count',
-    'weight',
-    'width',
-    'depth',
-    'action'
-  ];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  clickedRows = new Set<PeriodicElement>();
 
   ngOnInit(): void {
     console.log('Order ID geldi:', this.order_id);
